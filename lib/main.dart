223cd703dce3ebb99e65de449b1b1233389b1319
jaysardhara34 <jack.sardhara01@gamil.com>
2022-08-23 19:20:09 +0530

    New Proj

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:invoicegenerator/screens/userscreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "user",
+      routes: {
+        "/": (context) => Splashscreen(),
+        "user": (context) => Userscreen(),
+      },
+    ),
+  );
+}
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/userscreen.dart
@@ -0,0 +1,385 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Userscreen extends StatefulWidget {
+  const Userscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Userscreen> createState() => _UserscreenState();
+}
+
+class _UserscreenState extends State<Userscreen> {
+  List<Products> prod = [];
+  TextEditingController txtpname = TextEditingController();
+  TextEditingController txtpqty = TextEditingController();
+  TextEditingController txtpprice = TextEditingController();
+  TextEditingController txtpdisc = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff3f3f3f),
+        appBar: AppBar(
+          elevation: 00,
+          leading: Icon(Icons.home),
+          title: Text('User Screen'),
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Icon(Icons.more_vert_rounded),
+            )
+          ],
+          backgroundColor: Color(0xff3f3f3f),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Username :",
+                  style: TextStyle(color: Color(0xffffffff), fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: 350,
+                  child: TextField(
+                    style: TextStyle(color: Color(0xffffffff)),
+                    decoration: InputDecoration(
+                      enabledBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusedBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusColor: Color(0xffffffff),
+                      hintText: 'Enter Username',
+                      hintStyle: TextStyle(
+                        color: Color(0xff989898),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Address :",
+                  style: TextStyle(color: Color(0xffffffff), fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: 350,
+                  child: TextField(
+                    style: TextStyle(color: Color(0xffffffff)),
+                    decoration: InputDecoration(
+                      enabledBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusedBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusColor: Color(0xffffffff),
+                      hintText: 'Enter Address',
+                      hintStyle: TextStyle(
+                        color: Color(0xff989898),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Contact No :",
+                  style: TextStyle(color: Color(0xffffffff), fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: 350,
+                  child: TextField(
+                    style: TextStyle(color: Color(0xffffffff)),
+                    decoration: InputDecoration(
+                      enabledBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusedBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusColor: Color(0xffffffff),
+                      hintText: 'Enter Contact No.',
+                      hintStyle: TextStyle(
+                        color: Color(0xff989898),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Payment method :",
+                  style: TextStyle(color: Color(0xffffffff), fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: 350,
+                  child: TextField(
+                    style: TextStyle(color: Color(0xffffffff)),
+                    decoration: InputDecoration(
+                      enabledBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusedBorder: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.white54),
+                      ),
+                      focusColor: Color(0xffffffff),
+                      hintText: 'Enter Paymentmathod',
+                      hintStyle: TextStyle(
+                        color: Color(0xff989898),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Product details",
+                    style: TextStyle(color: Color(0xffffffff), fontSize: 22),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: txtpname,
+                      style: TextStyle(color: Color(0xffffffff)),
+                      decoration: InputDecoration(
+                        enabledBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusColor: Color(0xffffffff),
+                        hintText: 'Product name',
+                        hintStyle: TextStyle(
+                          color: Color(0xff989898),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: txtpqty,
+                      keyboardType: TextInputType.numberWithOptions(),
+                      style: TextStyle(color: Color(0xffffffff)),
+                      decoration: InputDecoration(
+                        enabledBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusColor: Color(0xffffffff),
+                        hintText: 'Qty',
+                        hintStyle: TextStyle(
+                          color: Color(0xff989898),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: txtpprice,
+                      keyboardType: TextInputType.numberWithOptions(),
+                      style: TextStyle(color: Color(0xffffffff)),
+                      decoration: InputDecoration(
+                        enabledBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusColor: Color(0xffffffff),
+                        hintText: 'Price',
+                        hintStyle: TextStyle(
+                          color: Color(0xff989898),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: txtpdisc,
+                      keyboardType: TextInputType.numberWithOptions(),
+                      style: TextStyle(color: Color(0xffffffff)),
+                      decoration: InputDecoration(
+                        enabledBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusedBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white54),
+                        ),
+                        focusColor: Color(0xffffffff),
+                        hintText: 'Discount',
+                        hintStyle: TextStyle(
+                          color: Color(0xff989898),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Products pdct = Products(
+                      Pdetail: txtpname.text,
+                      qty: txtpqty.text,
+                      price: txtpprice.text,
+                      discount: txtpdisc.text);
+                  prod.add(pdct);
+                  setState(() {
+                    txtpname = TextEditingController(text: '');
+                    txtpqty = TextEditingController(text: '');
+                    txtpprice = TextEditingController(text: '');
+                    txtpdisc = TextEditingController(text: '');
+                  });
+                },
+                child: Container(
+                  width: 70,
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.add_box_rounded, color: Color(0xff000000)),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Add",
+                        style: TextStyle(
+                            color: Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      )
+                    ],
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(primary: Color(0xfffd00d3)),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: prod.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          "${prod[index].Pdetail}",
+                          style:
+                              TextStyle(color: Color(0xffffffff), fontSize: 18),
+                        ),
+                        leading: Text(
+                          "${prod[index].qty}",
+                          style:
+                              TextStyle(color: Color(0xff989898), fontSize: 13),
+                        ),
+                        subtitle: Text(
+                          '${prod[index].discount}',
+                          style:
+                              TextStyle(color: Color(0xff707070), fontSize: 15),
+                        ),
+                        trailing: Text(
+                          '${prod[index].price}',
+                          style:
+                              TextStyle(color: Color(0xffffffff), fontSize: 15),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Products {
+  String? Pdetail, qty, price, discount;
+
+  Products({this.Pdetail, this.qty, this.price, this.discount});
+}
