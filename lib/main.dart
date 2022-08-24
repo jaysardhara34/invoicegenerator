296047612e6e522e26a711d498b904bdce85a178
jaysardhara34 <jack.sardhara01@gamil.com>
2022-08-24 14:35:56 +0530

    24/08/2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:invoicegenerator/screens/userscreen.dart';
@@ -6,7 +7,6 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "user",
       routes: {
         "/": (context) => Splashscreen(),
         "user": (context) => Userscreen(),
@@ -23,8 +23,46 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashscreenState extends State<Splashscreen> {
+  double i = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, 'user');
+    });
+    Timer.periodic(Duration(milliseconds: 300), (timer) {
+      if (i < 100) {
+        setState(() {
+          i++;
+        });
+      }
+    });
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff3f3f3f),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(backgroundImage:AssetImage('assets/logo.png'),radius: 150,),
+              SizedBox(
+                height: 30,
+              ),
+              CircularProgressIndicator(color: Color(0xff000000),
+                backgroundColor: Color(0xffbebebe),
+                value: i / 100,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "$i%",
+                style: TextStyle(color: Color(0xffffffff), fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
